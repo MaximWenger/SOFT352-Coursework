2c0fbbefa94f4d49d5ab412cbcc972931dc3c3e0
HawkBlues <HawkBlues@users.noreply.github.com>
2018-12-21 02:38:11 +0000

    Multiple clients now stored
    Now need to address each client invidually. New object needed for each game,.

--- client.js
+++ client.js
@@ -1,23 +1,15 @@
-//Import the module.
 var WebSocketClient = require("websocket").client;
 
-//Instantiate a client
 var client = new WebSocketClient();
 
+client.on("connect", function(connection) {
+  console.log("WebSocket connected");
 
+  connection.on("message", function(message) {
+    console.log("Received: " + message.utf8Data);
+  });
 
-client.on("connect", function(connection){
-//set up an event handler to process messages received from the server
-
-
-connection.on("message", function(message){
-	var d = new Date();
-	var n = d.toLocaleTimeString();
-	
-console.log("Received: " + message.utf8Data + " " + n + "\n");
+  connection.sendUTF("Hello world, from the client");
 });
 
-//When the connection is established, send the server a message
-connection.sendUTF("Handshake from Client");
-});
 client.connect("ws://localhost:9000");
--- server.js
+++ server.js
@@ -1,50 +1,54 @@
-//Import the requred modules.
 var WebSocketServer = require("websocket").server;
 var http = require("http");
 var readline = require("readline");
+const uuidv4 = require('uuid/v4'); //produces random number
 
-//Instantitate a socket object.
-var server = http.createServer(function(request, response){
-console.log(new Date() + " Recieved request");
+var server = http.createServer(function(request, response) {
+  console.log(new Date() + " Received request");
+});
+server.listen(9000, function() {
+  console.log(new Date() + " listening on port 9000");
+});
+
+wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+var connections = {}; //Factory pattern for each new connection
+
+wsServer.on("request", function(request) {
+  var connection = request.accept(null, request.origin);
+  var id = uuidv4(); //assigns random number as the ID
+
+  connection.on("close", function(reasonCode, description) {
+    console.log("Closing a connection");
+    delete connections[id];   // Delete the connection from the object once the client disconnects.
+  });
+
+  // Add the new connection to the array of connections.
+  connections[id] = connection;
+  for (var id in connections) {
+    connections[id].sendUTF("A new connection was made - now " + Object.keys(connections).length + " connected clients (" + connection.id + ")");
+  }
 });
 
 var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-server.listen(9000, function(){
-console.log(new Date() + " listening on port 9000");
+function askQuestion() {
+  rl.question("Enter a message ('quit' to end)", function(answer) {
+    if (answer === 'quit') {
+      return rl.close();
+    }
 
+    for (var id in connections) {
+      connections[id].sendUTF("New message (obj): '" + answer + "'");
+    }
 
-});
-var socket = new WebSocketServer({httpServer: server});
+    askQuestion();
+  });  
+}
 
-
-
-
-socket.on("request", function(request){
-	var connection = request.accept(null, request.origin);
-	
-	connection.on("close", function(reasonCode, description){
-		console.log("Connection closed");
-	});
-	
-
-	connection.on("message", function(message){
-		console.log(message.utf8Data);
-	console.log("Handshake from server \n");
-	
-		//connection.sendUTF(message.utf8Data);
-		
-	
-});
-
-	rl.question("Enter message for client \n", function(answer){ //While BOOL x is false, constantly call this function. If ESC is pressed, BOOL == true;
-		connection.sendUTF(answer);
-	console.log("Sent message: ", answer, " :to client");
-	
-});
-	
-
-});
+askQuestion();
