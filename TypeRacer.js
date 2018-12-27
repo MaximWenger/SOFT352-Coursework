30c716bac43285bcef7d13c144244b76384ac8ee
HawkBlues <HawkBlues@users.noreply.github.com>
2018-12-27 02:54:19 +0000

    Added online/offline indication

--- TypeRacer.js
+++ TypeRacer.js
@@ -2,18 +2,46 @@
 
 var WebSocketClient = new WebSocket("ws://localhost:9000");
 
-//var client = new WebSocketClient();
-
-WebSocketClient.onopen = function(event){ //When connection is on, do this
-	WebSocketClient.send("Hello world from the client");
-	alert('Socket connected succes');
-};
-
 WebSocketClient.onmessage = function(message){ //When get a message, do this
     console.log("Received: " + message.data);
     alert(message.data);
 }
 
+WebSocketClient.onerror = function (event){
+	console.log("Error connecting to server =" + event);
+	
+}
+
+function connectToServer(){
+//var client = new WebSocketClient();
+var connected = false;
+WebSocketClient.onopen = function(event){ //When connection is on, do this
+	WebSocketClient.send("Hello world from the client");
+	alert('Socket connected succes');
+	connected = true;
+
+}
+return connected;
+}
+
+
+
+
 $(document).ready(function(){
+	connectToServer();
+
+	
+$(".dot1").click(function () {
+alert('Click is working!');
+});
+$(".dot2").click(function () {
+alert('Click is working!');
+});
+$(".dot3").click(function () {
+alert('Click is working!');
+});
+$(".dot4").click(function () {
+alert('Click is working!');
+});
 
 });
