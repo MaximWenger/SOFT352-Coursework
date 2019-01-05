759a690409d18d20f78326051f007d1b52f3456d
HawkBlues <HawkBlues@users.noreply.github.com>
2019-01-05 01:40:19 +0000

    Update server.js
    Added Ticker and started working on main engine.
    
    Issues: Unable to find players.length, within Main() (Called via ticker) to confirm > 2 players are online.

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ wsServer = new WebSocketServer({
 });
 
 var connections = {}; //Factory pattern for each new connection
-var players = {} // To store all current users
+var players = {}; // To store all current users
+var games = {}; //To store all current games
 
 wsServer.on("request", function(request) {
   var connection = request.accept(null, request.origin);
@@ -35,9 +36,12 @@ wsServer.on("request", function(request) {
     connections[id].sendUTF("A new connection was made - now " + Object.keys(connections).length + " connected clients (" + connection.id + ")");
   }
 
-    connection.on("message", function incoming(request) {
+    connection.on("message", function incoming(request) {		
         console.log(request.utf8Data);
-        createPlayer(request, id);
+
+		if (identifyMsg(request) == "player"){ //Confirming it's a new player joining			
+			createPlayer(request, id);
+		};
     });
 
 });
@@ -47,27 +51,72 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
-function askQuestion() {
-  rl.question("Enter a message ('quit' to end)", function(answer) {
-    if (answer === 'quit') {
-      return rl.close();
-    }
+//function askQuestion() {
+ // rl.question("Enter a message ('quit' to end)", function(answer) {
+ //   if (answer === 'quit') {
+ //     return rl.close();
+ //   }
+//
+//    for (var id in connections) {
+//      connections[id].sendUTF("New message (obj): '" + answer + "'");
+//    }
 
-    for (var id in connections) {
-      connections[id].sendUTF("New message (obj): '" + answer + "'");
-    }
-
-    askQuestion(); //Never ending loop
-  });  
+//    askQuestion(); //Never ending loop
+ // });  
+//}
+function identifyMsg(message){//Used to identify the type of message sent from client
+	var fullMsg = message.utf8Data;
+	if (fullMsg.includes("~")){//Used to identfy is the message is a new user settings being recieved
+		return "player";
+	}
+}
+function startTicker(){
+	console.log("Ticker ON");
+	var timer = setInterval(main, 1000); //Calls the main function once a second.
 }
 
 
-function createPlayer(message, id){
+function createPlayer(message, id){ //Creates new user object, using the users color and name
+console.log("Creating player")
 var color = getColor(message);
 var name = getUserName(message);
     players[id] = playerFactory.create(color, name, id); //Creates new playerr object
-    console.log(players[id].color);
+    console.log(players[id].name + "Created");
 }
+
+
+function createGame(player1, player2){ //Creates a new Game object 
+	var GId = uuidv4(); //Produces random ID
+	games[GId] = gameFactory.create(player1, player2); //Creates new gameID
+	
+}
+
+function beginGame(){
+	var counter = 0;
+	var P1;
+	var P2;
+	for (var i = 0; i < players.length; i++){ //Find the first two available players
+		if (players[i].playing == 'N' || counter < 2){
+			if (counter == 0){
+			P1 = i;
+			counter++;
+			}
+			else if (counter == 1){
+				P2 = i;
+				counter++;
+				break;
+			}
+		}
+	}
+	console.log(players[P1].playing);
+	console.log(players[P2].playing);
+	players[P1].playing = 'Y';
+	players[P2].playing = 'Y';
+		console.log(players[P1].playing);
+	console.log(players[P2].playing);
+	//createGame(players[P1], players[P2]);
+}
+
 function getColor(message){ //Returns the user chosen colour
 	var x = [];
 	x = message.utf8Data;
@@ -103,7 +152,8 @@ playerFactory = { //Pratical 3 | Used to create new players
 		p = {
 		'color': pColor,
 		'name': pName,
-		'Id': pId
+		'Id': pId,
+		'playing':'N'
 		}
 	return p;
 	}
@@ -123,4 +173,16 @@ gameFactory = { //Used to create new game objects, using player objects
 	}
 };
 
-askQuestion();
+
+
+function main(){
+	console.log("Tick");
+	console.log(players.length + ":Player length");
+	if (players.length >= 2){
+		console.log("Got to Main!")
+		beginGame();
+	}
+}
+
+startTicker();
+
